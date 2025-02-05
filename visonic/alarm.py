2fcf24e9242d81a5b988e69890866def9150e8ae
Andrea <31848430+And3rsL@users.noreply.github.com>
2025-02-05 20:29:46 +0100

    Update alarm.py

--- visonic/alarm.py
+++ visonic/alarm.py
@@ -227,7 +227,7 @@ class System(object):
             print('Rest API version 10.0 is supported.')
         elif '12.0' in rest_versions:
             print('Rest API version 12.0 is supported.')
-            self.__api.setVersionUrls('10.0')
+            self.__api.setVersionUrls('12.0')
         else:
             raise Exception(f'Rest API version 8.0, 9.0 or 10.0 is not supported by server. Supported versions: {", ".join(rest_versions)}')
 
