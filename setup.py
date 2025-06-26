a38c08c0463357b9d17c5ed299bfee6b0678faf9
Andrea <31848430+And3rsL@users.noreply.github.com>
2025-06-26 23:37:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="visonicalarm2",
-    version="3.3.0",
+    version="3.3.1",
     author="Andrea Liosi",
     author_email="andrea.liosi@gmail.com",
     description="A simple API library for the Visonic/Bentel/Tyco Alarm system.",
